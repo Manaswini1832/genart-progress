c1b9b42a5b95ab697030c9f64d9ab439493c4db5
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2020-09-17 05:34:19 +0530

    Drawing with text (used the - symbol)

--- sketch.js
+++ sketch.js
@@ -28,12 +28,13 @@ const sketch = () => {
         let u = count <= 1 ? 0.5 : x / (count - 1);
         let v = count <= 1 ? 0.5 : y / (count - 1);
 
-        const radius = Math.abs(random.noise2D(u, v) * 0.025);
+        const radius = Math.abs(random.noise2D(u, v) * 0.5);
 
         points.push({
           color: random.pick(palette),
           position: [u, v],
           radius,
+          rotation: random.noise2D(x, y),
         });
       }
     }
@@ -42,24 +43,32 @@ const sketch = () => {
 
   // random.setSeed(512); //Adds deterministic randomness
   const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 300;
+  const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = "#fff";
     context.fillRect(0, 0, width, height);
 
     points.forEach((data) => {
-      const { color, position, radius } = data;
+      const { color, position, radius, rotation } = data;
 
       const [u, v] = position;
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Arial"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText("-", 0, 0);
+      context.restore();
     });
   };
 };
